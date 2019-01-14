f6e39f8761ac80b60ba0032ed971ca16b65bc855
Illuminatus <theilluminatus175@gmail.com>
2019-01-14 20:05:18 +0100

    further updates to eh-torrent-crawler

--- eh-torrent-crawler.js
+++ eh-torrent-crawler.js
@@ -3,11 +3,15 @@
 // 
 // Just copy paste this file into the dev console of the page you want to crawl (only tested on Chrome)
 // The script logs the a json output of this form: 
-//   [{id: "0000000", link: "https://ehtracker.org/get/id/hash.torrent", seeders: "0"}]
+//   [{id: "0000000", link: "https://ehtracker.org/get/id/hash.torrent", filename: "name.zip", seeders: "0"}]
 
-var galleries = [];
+var galleries = [], count = 0;
 Array.from(document.querySelectorAll(".itg tr[class]")).map((g) => { // get all galleries
-  return { "id": Number(g.querySelector(".it2").id.substr(1)), "link": (g.querySelector(".i a") ? g.querySelector(".i a").href:undefined) }
+  count++;
+  return {
+    "id": Number(g.querySelector(".it2").id.substr(1)),
+    "link": (g.querySelector(".i a") ? g.querySelector(".i a").href:undefined)
+  }
 }).forEach(g => { 
   if (g.link) { // request list of torrents
     var req = new XMLHttpRequest() 
@@ -18,19 +22,20 @@ Array.from(document.querySelectorAll(".itg tr[class]")).map((g) => { // get all 
         Array.from(htmlDoc.querySelectorAll("form:not([enctype])")).map((f) => {
           return { id: g.id,
             link: f.querySelector("a").href,
+            filename: f.querySelector("a").textContent,
             seeders: Number(Array.from(f.querySelector("td:nth-child(4)").childNodes)
               .filter(c => c.nodeType === Node.TEXT_NODE)
               .map(t => t.textContent)
               .join().trim())
           }
-        }).sort((a, b) => a.seeders > b.seeders)[0]
+        }).sort((a, b) => b.seeders - a.seeders)[0]
       )
-      if (galleries.length > 49) { console.log(JSON.stringify(galleries)); console.table(galleries) }
+      if (galleries.length >= count) { console.log(JSON.stringify(galleries)); console.table(galleries) }
     })
     req.open("GET", g.link)
     req.send()
   } else {
     galleries.push(g);
-    if (galleries.length > 49) { console.log(JSON.stringify(galleries)); console.table(galleries) }
+    if (galleries.length >= count) { console.log(JSON.stringify(galleries)); console.table(galleries) }
   }
 })
