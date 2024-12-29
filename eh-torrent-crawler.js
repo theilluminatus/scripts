fa266092ffd5e29281d2480ca0665106567fccfa
Illuminatus <theilluminatus175@gmail.com>
2024-12-29 21:47:08 +0100

    Update

--- eh-torrent-crawler.js
+++ eh-torrent-crawler.js
@@ -6,23 +6,27 @@
 //   [{id: "0000000", link: "https://ehtracker.org/get/id/hash.torrent", filename: "name.zip", seeders: "0"}]
 
 var galleries = [], count = 0;
-Array.from(document.querySelectorAll(".itg tr[class]")).map((g) => { // get all galleries
+Array.from(document.querySelectorAll("table.itg > tbody > tr")).map((g) => { // get all galleries
   count++;
   return {
-    "id": Number(g.querySelector(".it2").id.substr(1)),
-    "link": (g.querySelector(".i a") ? g.querySelector(".i a").href:undefined)
+    "id": Number(g.querySelector("div[id^=posted_]").id.replace("posted_", "")),
+    "torrent": (g.querySelector(".gldown a") ? g.querySelector(".gldown a").href : undefined),
+    "name": g.querySelector(".glink").textContent,
+    "link": (g.querySelector(".gl1e a") ? g.querySelector(".gl1e a").href : undefined)
   }
-}).forEach(g => { 
-  if (g.link) { // request list of torrents
+}).forEach((g, i) => { 
+  if (g.torrent) { // request list of torrents
     var req = new XMLHttpRequest() 
     req.addEventListener("load", (e) => {
       var parser = new DOMParser();
-      var htmlDoc = parser.parseFromString(e.target.response, 'text/xml');
+      var htmlDoc = parser.parseFromString(e.target.response, 'text/html');
       galleries.push( // get all torrents and sort them by seeds -> add first / best torrent
         Array.from(htmlDoc.querySelectorAll("form:not([enctype])")).map((f) => {
-          return { id: g.id,
-            link: f.querySelector("a").href,
-            filename: f.querySelector("a").textContent,
+          return {
+            id: g.id,
+            torrent: f.querySelector("a").href,
+            name: f.querySelector("a").textContent,
+            link: g.link,
             seeders: Number(Array.from(f.querySelector("td:nth-child(4)").childNodes)
               .filter(c => c.nodeType === Node.TEXT_NODE)
               .map(t => t.textContent)
@@ -32,7 +36,7 @@ Array.from(document.querySelectorAll(".itg tr[class]")).map((g) => { // get all 
       )
       if (galleries.length >= count) { console.log(JSON.stringify(galleries)); console.table(galleries) }
     })
-    req.open("GET", g.link)
+    req.open("GET", g.torrent)
     req.send()
   } else {
     galleries.push(g);
